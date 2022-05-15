552903a56cef2c6ee15541b8743dfb4814bb6b02
Adrian <adrianbeys@wp.pl>
2022-05-15 19:07:18 +0200

    Added routes, html, css files

--- testy/routes.py
+++ testy/routes.py
@@ -2,6 +2,10 @@ from flask import redirect, render_template, url_for, flash, request
 from testy import app
 
 @app.route("/")
-@app.route("/index")
+@app.route("/home")
 def home_page():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/dashboard")
+def dashboard_page():
+    return render_template("dashboard.html")
