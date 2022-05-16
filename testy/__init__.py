fc5eedfe68607241d9d5a064241ae4f4622954b1
Mateusz Piekny <m.piekny97@gmail.com>
2022-05-16 10:10:29 +0200

    Fixed navbar, added jinja injection functionality

--- testy/__init__.py
+++ testy/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
--- testy/routes.py
+++ testy/routes.py
@@ -1,10 +1,10 @@
 from flask import redirect, render_template, url_for, flash, request
 from testy import app
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 def home_page():
-    return render_template("home.html")
+    return render_template("home.html", is_authenticated=True)
 
 @app.route("/dashboard")
 def dashboard_page():
