6dcc0f7d6aa1e1b97e2fad6c80b08d7aa128c96e
Adrian <adrianbeys@wp.pl>
2022-05-16 16:15:21 +0200

    Added login page etc.

--- testy/routes.py
+++ testy/routes.py
@@ -4,8 +4,13 @@ from testy import app
 @app.route("/", methods=["GET", "POST"])
 @app.route("/home", methods=["GET", "POST"])
 def home_page():
-    return render_template("home.html", is_authenticated=True)
+    return render_template("home.html", is_authenticated=False)
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html", is_authenticated=False)
 
 @app.route("/dashboard")
 def dashboard_page():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", is_authenticated=True)
+
