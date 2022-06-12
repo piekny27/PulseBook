9d68c09ab56185c50d30d322edaf10b02f94ff9a
Mateusz Piekny <m.piekny97@gmail.com>
2022-06-12 21:33:46 +0200

    Implement postgres feature

--- testy/__init__.py
+++ testy/__init__.py
@@ -6,7 +6,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DataBase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uvvdtmrpliizua:ae4230cf848b72c0cf627c0745b27e18b92d9dbec4e138c82f75216d900734e5@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d5i2ue6f3o4r9j'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
