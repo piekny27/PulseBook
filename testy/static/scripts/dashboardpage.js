438039dda41e6c57052d79ba5abd9e863b6a08f4
Mateusz Piekny <m.piekny97@gmail.com>
2022-06-28 20:56:22 +0200

    Updated bmi_card

--- testy/static/scripts/dashboardpage.js
+++ testy/static/scripts/dashboardpage.js
@@ -4,7 +4,8 @@ $( document ).ready(function() {
     $('.ui-draggable').draggable({ // Make cards draggable
         stack: '.card', // Make dragged card appear above the others
         revert: false, // Make dragged card return to start position
-        handle: ".card-header"
+        handle: ".card-header",
+        containment: ".card-block",
+        snap:[10,10]
       })
-         
  });
