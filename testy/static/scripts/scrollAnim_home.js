8d3562db064fa847212b8a0f76928a5b7cce3b4f
Mateusz Piekny <m.piekny97@gmail.com>
2022-07-08 18:03:38 +0200

    Added second section in homepage

--- testy/static/scripts/scrollAnim_home.js
+++ testy/static/scripts/scrollAnim_home.js
@@ -2,7 +2,6 @@ const html = document.documentElement;
 const canvas = document.getElementById("renderCanvas");
 const context = canvas.getContext("2d");
 const firstScreen = document.getElementById("firstScreen");
-const brandName = document.getElementById("brandName");
 
 const frameCount = 148;
 const currentFrame = index => (
@@ -46,12 +45,58 @@ function setBgColor(scrollValue){
   firstScreen.style.background = `radial-gradient(circle, rgba(${r2}, ${g2}, ${b2}, 1) 0%,rgba(${r}, ${g}, ${b}, 1) 65%, rgba(${r}, ${g}, ${b}, 1) 100%)`
 }
 
+function animText(scrollValue, elementID, startScroll, stopScroll){
+  const text1 = document.getElementById(elementID);
+  if(scrollValue>startScroll && scrollValue<stopScroll){
+    y=map_range(scrollValue,startScroll,stopScroll,40,-40);
+    if(scrollValue>startScroll && scrollValue < startScroll + ((stopScroll-startScroll)/3))
+    {
+      text1.style.opacity=map_range(scrollValue,startScroll,startScroll + ((stopScroll-startScroll)/3),0,1);
+    }
+    if(scrollValue>startScroll + ((stopScroll-startScroll)/3) && scrollValue < startScroll + 2*((stopScroll-startScroll)/3))
+    {
+      text1.style.opacity=1;
+    }
+    if(scrollValue>startScroll + 2*((stopScroll-startScroll)/3) && scrollValue < startScroll + 3*((stopScroll-startScroll)/3))
+    {
+      text1.style.opacity=map_range(scrollValue,startScroll+ 2*((stopScroll-startScroll)/3),startScroll + 3*((stopScroll-startScroll)/3),1,0);
+    }
+    text1.style.transform=`translate(-50%, calc(-50% + ${y}px))`;
+  }
+  else{
+    text1.style.opacity=0;
+  }
+}
+
+function animRenderOut(scrollValue){
+  if(scrollValue>0.9){
+    y=map_range(scrollValue,0.9,1.4,-50,-200);
+    canvas.style.transform=`translate(-50%,${y}%)`;
+  }else
+  {
+    canvas.style.transform=`translate(-50%,-50%)`;
+  };
+};
+
+function animBrandTextOut(scrollValue){
+  const brandText = document.getElementById("brandText");
+  if(scrollValue<0.5){
+    y=map_range(scrollValue,0,0.5,0,-200);
+    brandText.style.transform=`translateY(${y}px)`;
+  };
+};
+
 window.addEventListener('scroll', () => {  
   const scrollTop = html.scrollTop;
   const maxScrollTop = firstScreen.scrollHeight - window.innerHeight;
   const scrollFraction = scrollTop / maxScrollTop;
   setBgColor(scrollFraction);
-
+  animRenderOut(scrollFraction);
+  animBrandTextOut(scrollFraction);
+  animText(scrollFraction,'text1', 1.2, 1.9);
+  animText(scrollFraction,'text2', 1.9, 2.6);
+  animText(scrollFraction,'text3', 2.6, 3.3);
+  console.log(scrollFraction);
   const frameIndex = Math.min(
     frameCount - 1,
     Math.ceil(scrollFraction * frameCount)
@@ -61,6 +106,6 @@ window.addEventListener('scroll', () => {
 });
 
 $("#renderCanvas").delay(200).fadeTo(1500,1);
-$("#brandName").delay(1000).fadeTo(2000,1);
+$("#brandText").delay(1000).fadeTo(2000,1);
 preloadImages();
 
